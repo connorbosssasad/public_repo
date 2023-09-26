ab7a52e892e4ecb1adfae86ef17bfb67b7e5bb30
connorbosssasad <144817460+connorbosssasad@users.noreply.github.com>
2023-09-26 14:25:21 +0100

    commit.

---
+++ space_project.py
@@ -0,0 +1,16 @@
+
+print("Hello, my name is Susan from Space.")
+name = input("What is your name?: ")
+
+print(f"Hello {name} I am from the year 2210 and i am 20 years old")
+
+age = input("How old are you?:  ")
+
+future_age = int(age) + 2210 - 2023
+
+print(f"Wow, by 2210 you will be {future_age}, that is really old!!!!")
+
+print("What music are you into")
+music = input("Enter music: ")
+
+print(f"I have not heard of {music}")
